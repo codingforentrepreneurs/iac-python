3d746f0dede6b2fa400e9f5b07621912e4a83f6d
Coding For Entrepreneurs <codingforentrepreneurs@gmail.com>
2021-10-14 14:35:43 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def home_view():
-    return  {"hello": "world", "what": "iac is amazing"}
+    return  {"hello": "world", "msg": "iac is amazing"}
 
 
 @app.post("/")
